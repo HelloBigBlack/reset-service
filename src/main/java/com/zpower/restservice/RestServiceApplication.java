90af6f9c64ab85545dad73b0f79243a853e0dba3
ZR19050108-郑亚雄 <zhengyaxiong@zhongruigroup.com>
2020-12-17 09:06:40 +0800

    fix: 修改类的命名

--- src/main/java/com/zpower/restservice/RestServiceApplication.java
+++ src/main/java/com/zpower/restservice/RestServiceApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-public class RestserviceApplication {
+public class RestServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestserviceApplication.class, args);
+        SpringApplication.run(RestServiceApplication.class, args);
     }
 
 }
